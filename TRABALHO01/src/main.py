5c1fdf736b6929b44e13bc01674bc8000773031b
Thiago <tharbuin@gmail.com>
2023-08-31 15:06:57 -0300

    Continuando trabalho

--- TRABALHO01/src/main.py
+++ TRABALHO01/src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from logging.config import dictConfig
 import logging
@@ -9,25 +9,53 @@ dictConfig(log_config)
 app = FastAPI(debug=True)
 logger = logging.getLogger('foo-logger')
 
+# Lista para armazenar os alunos simulados
+alunos_db = []
+
 class Aluno(BaseModel):
     nome: str
     matricula: str
     curso: str
-    
 
+# Rota principal
 @app.get("/")
 async def root():
     logger.debug("Aqui na rota principal")
-    mensagem = {"response" : "Arquitetura de software aplicada"}
+    mensagem = {"response" : "Trabalho 01 - ASA"}
     return mensagem
 
-@app.get("/parametro/{parametro_id}")
-async def mostra_parametro(parametro_id):
-    mensagem = f"O valor do paramento - {parametro_id}"
-    logger.info("Foi enviado um parametro correto!!!")
-    return {"response" : mensagem}
+# Rota para obter a lista de alunos (GET) no alunos_db
+@app.get("/alunos/")
+def get_alunos():
+    return {"alunos": alunos_db}
 
-@app.post("/alunos")
-async def criar_aluno(aluno: Aluno):
+# Rota para criar um novo aluno (POST) e adicionar no alunos_db
+@app.post("/alunos/")
+def create_aluno(aluno: Aluno):
     logger.error('ERROR')
-    return aluno
+    alunos_db.append(aluno)
+    return {"message": "Aluno cadastrado com sucesso", "aluno": aluno}
+
+# Rota para atualizar um aluno existente (PUT)
+@app.put("/alunos/")
+def update_aluno(aluno_matricula, aluno: Aluno):
+    logger.error('ERROR')
+    for aluno_ in alunos_db:
+        if aluno_.matricula == aluno_matricula:
+            alunos_db.remove(aluno_)
+            alunos_db.append(aluno)
+            return {"message": "Aluno atualizado com sucesso", "aluno": aluno}
+            
+
+# Rota para excluir um aluno (DELETE)
+@app.delete("/alunos/")
+def delete_aluno(aluno_matricula: str):
+    logger.error('ERROR')
+    for aluno in alunos_db:
+        if aluno.matricula == aluno_matricula:
+            alunos_db.remove(aluno)
+            return {"message": "Aluno removido com sucesso", "aluno": aluno_matricula}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
