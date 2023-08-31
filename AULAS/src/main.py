8e90265d9a040984c2ae98ba7790438350530f81
Thiago <tharbuini@gmail.com>
2023-08-31 11:18:11 -0300

    Adicionando arquivos da aula

--- AULAS/src/main.py
+++ AULAS/src/main.py
@@ -1,27 +1,33 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from logging.config import dictConfig
+import logging
+from src.config import log_config
 
-app = FastAPI()
+dictConfig(log_config)
+
+app = FastAPI(debug=True)
+logger = logging.getLogger('foo-logger')
 
 class Aluno(BaseModel):
-    nome : str
-    matricula : str
-    curso : str
+    nome: str
+    matricula: str
+    curso: str
+    
 
 @app.get("/")
 async def root():
-    mensagem = {"response" : "Ei Real Madrid"}
+    logger.debug("Aqui na rota principal")
+    mensagem = {"response" : "Arquitetura de software aplicada"}
     return mensagem
 
-@app.get("/parameter/{parameterID}")
-async def mostraParametro(parameterID):
-    mensagem = f"O valor do parametro: {parameterID}"
-    return {"response" : mensagem }
+@app.get("/parametro/{parametro_id}")
+async def mostra_parametro(parametro_id):
+    mensagem = f"O valor do paramento - {parametro_id}"
+    logger.info("Foi enviado um parametro correto!!!")
+    return {"response" : mensagem}
 
 @app.post("/alunos")
-async def criarAluno(aluno : Aluno):
+async def criar_aluno(aluno: Aluno):
+    logger.error('ERROR')
     return aluno
-
-# pip install fastapi
-# pip install uvicorn
-# uvicorn main:app --reload
