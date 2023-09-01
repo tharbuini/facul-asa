3708525daeb939dae47e7504ebbf26575d96b6aa
Thiago <tharbuini@gmail.com>
2023-09-01 08:44:04 -0300

    Criando models e melhorando o main

--- AULA04/src/main.py
+++ AULA04/src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request
-from classes.py import Request_Aluno
+from classes import Request_Aluno
+from models import Aluno, session
 
 app = FastAPI()
 
@@ -14,6 +15,15 @@ async def root():
 async def criar_aluno(request_aluno: Request_Aluno):
     aluno_json = request_aluno
     print(aluno_json.nome)
+    
+    aluno = Aluno(
+        nome     = aluno_json.nome,
+        email    = aluno_json.endereco,
+        cpf      = aluno_json.cpf,
+        endereco = aluno_json.endereco
+    )
+    session.add(aluno)
+    session.commit()
     
     return {
         "status" : "SUCESS",
