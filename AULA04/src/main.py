0e804b7b0a84c21f4aea2fcf6dc31bc71ad09b71
Thiago <tharbuini@gmail.com>
2023-09-01 09:05:30 -0300

    Finalizando a aula

--- AULA04/src/main.py
+++ AULA04/src/main.py
@@ -8,7 +8,16 @@ app = FastAPI()
 async def root():
     return {
         "status": "SUCESS",
-        "data": "NO DATA"
+        "data"  : "NO DATA"
+    }
+
+@app.get("/alunos")
+async def get_all_alunos():
+    alunos_query = session.query(Aluno)
+    alunos = alunos_query.all()
+    return {
+        "status": "SUCESS",
+        "data"  : alunos
     }
 
 @app.post("/alunos")
@@ -29,4 +38,31 @@ async def criar_aluno(request_aluno: Request_Aluno):
         "status" : "SUCESS",
         "data"   : aluno_json
     }
+
+@app.put("/alunos")
+async def alterar_aluno(request_aluno: Request_Aluno):
+    try:    
+        aluno_json = request_aluno
+        aluno_query = session.query(Aluno).filter(
+            Aluno.id==aluno_json.id
+        )
+        aluno = aluno_query.first()
+        print(aluno.nome)
+        aluno.nome = aluno_json.nome
+        aluno.cpf = aluno_json.cpf
+        aluno.email = aluno_json.email
+        aluno.endereco = aluno_json.endereco
+
+        session.add(aluno)
+        session.commit()
+
+        return {
+            "status": "SUCESS",
+            "data": aluno_json
+        }
     
+    except Exception as e:
+            return {
+                "status": "SUCESS",
+                "data": "ALUNO NÃO ENCONTRADO"
+            }
